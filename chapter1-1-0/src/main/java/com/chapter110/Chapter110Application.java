61253b945a566113b10b0eeaba00b981403ce078
lizhenzhong <lzzqqtxfh@163.com>
2018-07-10 11:27:41 +0800

    First Commit ！

---
+++ chapter1-1-0/src/main/java/com/chapter110/Chapter110Application.java
@@ -0,0 +1,12 @@
+package com.chapter110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter110Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter110Application.class, args);
+    }
+}
